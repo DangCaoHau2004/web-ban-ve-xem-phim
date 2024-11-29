b7ac8eced6f08a4a814048e8ad65c63a2f106a26
Onotoki <chucuongtn@gmail.com>
2024-11-29 14:13:34 +0700

    fixelse

--- information.php
+++ information.php
@@ -162,7 +162,9 @@
       $_SESSION['users']['sdt'] = $sdt;
       echo '<script>alert("Cập nhật thành công!");</script>';
     } else {
-      echo '<script>alert("Lỗi cập nhật: ' . $conn->error . '");</script>';
+      $_SESSION['ERR'] = "Lỗi đăng ký! Vui lòng thử lại sau!";
+      header("Location: ERR404.php");
+      exit(); //Kết thúc lệnh sau khi chuyển hướng
     }
 
     $stmt->close();
