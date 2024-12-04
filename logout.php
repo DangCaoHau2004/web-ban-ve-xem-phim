93d9846c6366bab5552dbda99487512444de3c9f
Đức Minh Vũ <vum9848@gmail.com>
2024-12-04 22:09:15 +0700

    fix location after logout

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy(); // Hủy bỏ tất cả các session
-header("Location: navbar.php"); // Chuyển hướng người dùng về trang navbar.php
+header("Location: index.php"); // Chuyển hướng người dùng về trang navbar.php
 exit(); // Kết thúc script
 ?>
